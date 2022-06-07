f0824128a0aca8577f38f4beae1edabd81910af3
jabbermcn <jabber_mcn@tut.by>
2022-06-07 09:32:50 +0300

    HW3.WALL-E edit

--- HW3.WALL-E.py
+++ HW3.WALL-E.py
@@ -1,1 +1,7 @@
 note = "In a distant, but not so unrealistic, future where mankind has abandoned earth because it has become covered with trash from products sold by the powerful multi-national Buy N Large corporation, WALLE, a garbage collecting robot has been left to clean up the mess. Mesmerized with trinkets of Earth's history and show tunes, WALLE is alone on Earth except for a sprightly pet cockroach. One day, EVE, a sleek (and dangerous) reconnaissance robot, is sent to Earth to find proof that life is once again sustainable."
+print(len(note))
+print(note.lower())
+replaced_note = note.replace('WALLE', 'WALL-E')
+print(replaced_note)
+count_earth = note.count('Earth')
+print(count_earth)
